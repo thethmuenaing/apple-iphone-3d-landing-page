5bda6595e50f6fda74e0389dd0be09886d47b528
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-06-12 10:05:12 +0630

    Quote Section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Quote from "./sections/Quote";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<Quote />
+		</>
+	);
 }
 
 export default App;
